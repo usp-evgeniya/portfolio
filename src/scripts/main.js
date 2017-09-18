447e8f7ba7cfccd0e4350671c8f68e7ae3e2dda8
usp-evgeniya <usp-evgeniya@yandex.ru>
2017-09-19 00:09:56 +0300

    welcome css finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,15 @@ var $ = require('jquery');
 $(document).ready(function () {
     
     $(function(){
-        var example = "Пример сборки";
-        console.log(example);
+        var mainIntro = $('.main__intro');
+        $('.intro__btn-auth').on('click',function (e) {
+            e.preventDefault();
+            mainIntro.addClass('active');
+        });
+
+        $('.form__button--home').on('click',function (e) {
+            e.preventDefault();
+            mainIntro.removeClass('active');
+        });
     });
 });
