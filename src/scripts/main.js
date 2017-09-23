5cc4aa9110c5d96aff1db8ada7c15f2143603574
usp-evgeniya <usp-evgeniya@yandex.ru>
2017-09-23 23:20:50 +0300

    flip effect, rem - adaptive preparations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,19 @@ var $ = require('jquery');
 $(document).ready(function () {
     
     $(function(){
-        var mainIntro = $('.main--intro');
         $('.intro__btn-auth').on('click',function (e) {
             e.preventDefault();
+            var elem = $(e.target),
+                mainIntro = elem.closest('.main--intro');
+
             mainIntro.addClass('active');
         });
 
         $('.form__button--home').on('click',function (e) {
             e.preventDefault();
+            var elem = $(e.target),
+                mainIntro = elem.closest('.main--intro');
+
             mainIntro.removeClass('active');
         });
     });
