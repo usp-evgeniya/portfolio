8b385c9ca05e2e8481aa463547a27c065b24bf6b
usp-evgeniya <usp-evgeniya@yandex.ru>
2017-10-02 17:12:00 +0300

    Adaptive check and small corrections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,10 @@ const hamburger = require('./modules/hamburger');
 const welcome = require('./modules/welcome');
 const googleMap = require('./modules/map');
 const skills = require('./modules/skills');
+const slider = require('./modules/slider');
 
 hamburger();
 welcome();
 googleMap();
 skills();
+slider();
---
+++ src/scripts/modules/slider.js
@@ -0,0 +1,28 @@
+module.exports = $(function(){
+    var slides = [
+        {
+        subtitle: "Лендинг для магазина бургеров Mr.Burger", 
+        techs: "HTML5, CSS3, JavaScript & jQuery, Ajax, Gulp",
+        link: "https://usp-evgeniya.github.io/burgers/",
+        img: "img/content/works/work1.png",
+        active: "active",
+        },
+        {
+        subtitle: "Интернет-магазин лабораторного оборудования и приборов", 
+        techs: "HTML5, CSS3, JavaScript & jQuery",
+        link: "#",
+        img: "img/content/works/work2.png",
+        active: "",
+        },
+        {
+        subtitle: "Лендинг для студии красоты Пион", 
+        techs: "HTML5, CSS3, JavaScript & jQuery",
+        link: "#",
+        img: "img/content/works/work3.png",
+        active: "",
+        }
+    ]
+
+    
+
+});
