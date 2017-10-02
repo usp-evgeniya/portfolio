6aeab4d7032324bd91e0c4d52e4f85d230b6ac35
usp-evgeniya <usp-evgeniya@yandex.ru>
2017-10-02 19:40:49 +0300

    Slider ready for check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,8 @@
 //const $ = require('jquery');
-const hamburger = require('./modules/hamburger');
+const hamburgerInit = require('./modules/hamburger');
 const welcome = require('./modules/welcome');
 const googleMap = require('./modules/map');
 const skills = require('./modules/skills');
-const slider = require('./modules/slider');
+const sliderInit = require('./modules/slider');
 
-hamburger();
-welcome();
-googleMap();
-skills();
-slider();
+sliderInit();
