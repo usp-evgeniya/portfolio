22f5ad73e52f49def77623b83f875805fdef2a00
usp-evgeniya <usp-evgeniya@yandex.ru>
2017-10-08 01:12:50 +0300

    Skills corrected on scroll

--- src/scripts/modules/parallax-intro.js
+++ src/scripts/modules/parallax-intro.js
@@ -1,5 +1,3 @@
-// https://www.youtube.com/watch?v=LJ5NDW7AxtE
-
 module.exports = function () {
 
     var parallaxBox = document.getElementById('parallax'),
@@ -9,17 +7,15 @@ module.exports = function () {
         var initialX = (window.innerWidth / 2) - e.pageX;
         var initialY = (window.innerHeight / 2) - e.pageY;
 
-        [].slice.call(layers).forEach(function (layer, index) {
+        Array.prototype.slice.call(layers).forEach(function (layer, index) {
             var
                 divider = index / 100,
                 positionX = initialX * divider,
                 positionY = initialY * divider,
-                bottomPosition = (window.innerHeight / 2) * divider,
                 transform2D = 'translate(' + positionX + 'px,' + positionY + 'px)',
                 image = layer.firstElementChild;
 
             layer.style.transform = transform2D;
-            image.style.bottom = '-' + bottomPosition + 'px';
         });
 
     };
--- src/scripts/modules/skills.js
+++ src/scripts/modules/skills.js
@@ -1,6 +1,7 @@
 var $ = require('jquery');
 
-module.exports = $(function(){
+/*module.exports = $(function(){
+
     $('.js-circle').on('mouseover touchstart',function (e) {
         e.preventDefault();
         var elem = $(e.target),
@@ -10,4 +11,52 @@ module.exports = $(function(){
         circleColored.css('stroke-dasharray', strokeDasharray + ' 282.6');
     });
 
+});*/
+
+module.exports = $(function(){
+    var animateSvg = (function() {
+        var checkDistance = function (scrollTop, elem) {
+            var offset = elem.offset().top;
+            var windowMargin = Math.ceil($(window).height() / 3);
+            var topBorder = offset - scrollTop - windowMargin;
+            var bottomEdge = elem.outerHeight(true) + offset;
+            var bottomBorder = scrollTop + windowMargin - bottomEdge;
+            //console.log(topBorder,bottomBorder);
+
+            return topBorder <=0 && bottomBorder <= 0;
+        }
+
+        return {
+            init : function() {
+                $(window).on('scroll', function(){
+                    var scrollTop =  $(window).scrollTop();
+
+                    if(checkDistance(scrollTop,$('.js-circle'))) {
+                        var items = $('.js-circle'),
+                            counter = 0;
+
+                        function each() {
+                            var item = items.eq(counter);
+                                circleColored = item.find('.js-circle__second'),
+                                percent = circleColored.attr('data-circle'),
+                                strokeDasharray = 2 * Math.PI * 45 * percent / 100;
+                                
+                                circleColored.css('stroke-dasharray', strokeDasharray + ' 282.6');
+
+                                counter++;
+
+                                setTimeout(each, 20);
+                        }
+
+                        each();
+
+                    };
+                });
+            }
+        }
+
+    }());
+
+    animateSvg.init();
+    
 });
