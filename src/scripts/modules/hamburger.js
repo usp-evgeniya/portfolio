13aafbab174d4ba97d2a62507b581b4a2f552bd1
usp-evgeniya <usp-evgeniya@yandex.ru>
2017-10-11 20:46:19 +0300

    parallax bug fixing delete folder

--- src/scripts/modules/hamburger.js
+++ src/scripts/modules/hamburger.js
@@ -4,7 +4,7 @@ module.exports = $(function(){
     var hamburger = $('.js-hamburger');
     $('.js-hamburger__btn').on('click',function (e) {
         e.preventDefault();
-        $('.js-hamburger__menu').slideToggle(700);
+        $('.js-hamburger-menu').slideToggle(700);
         hamburger.toggleClass('active');
         if(hamburger.hasClass('active')) {
             $('body, html').css("overflow", "hidden");
